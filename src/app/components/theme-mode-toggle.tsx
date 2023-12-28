13813d18f481886e431689f00b3f43f8c51ebb43
Vinicius Mendes <vinimendesdev@gmail.com>
2023-12-28 13:47:50 -0300

    feat: added ThemeModeToggle components

--- src/app/components/theme-mode-toggle.tsx
+++ src/app/components/theme-mode-toggle.tsx
@@ -1,40 +1,27 @@
 'use client';
 
-import * as React from 'react';
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import React from 'react';
+import { Switch } from '@nextui-org/react';
+import { MoonIcon } from './MoonIcon';
+import { SunIcon } from './SunIcon';
 import { useTheme } from 'next-themes';
 
-import { Button } from '@/app/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/app/components/ui/dropdown-menu';
+export default function ThemeModeToggle() {
+  const { theme, setTheme } = useTheme();
 
-export function ModeToggle() {
-  const { setTheme } = useTheme();
+  // Função para alternar o tema
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='icon'>
-          <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-          <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-          <span className='sr-only'>Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Switch
+      checked={theme === 'dark'}
+      onChange={toggleTheme}
+      size='sm'
+      color='warning'
+      startContent={<SunIcon />}
+      endContent={<MoonIcon />}
+    />
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { ModeToggle } from './components/theme-mode-toggle';
-
 export default function Home() {
   return (
     <main>
       <p>Page Home</p>
-      <ModeToggle />
     </main>
   );
 }
