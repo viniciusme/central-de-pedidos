f48c09ab7c1ba9bd13f7c654a63b32c5216e0b6d
Vinicius Mendes <vinimendesdev@gmail.com>
2023-12-28 16:58:33 -0300

    feat: added Hader component to LatestOrders page layout

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,12 @@
 const Header = () => {
   return (
-    <div>
-      <p>Header</p>
+    <div className='bg-brandFirst-500'>
+      <div className='container mx-auto'>
+        <nav className='flex items-center justify-between'>
+          <div>Logo</div>
+          <div>Sair</div>
+        </nav>
+      </div>
     </div>
   );
 };
