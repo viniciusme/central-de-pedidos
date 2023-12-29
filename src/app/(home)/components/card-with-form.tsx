6bc369380779fd612bf31ed0970c89ce6050bc08
Vinicius Mendes <vinimendesdev@gmail.com>
2023-12-29 11:47:45 -0300

    feat: added link in logo image

--- src/app/(home)/components/card-with-form.tsx
+++ src/app/(home)/components/card-with-form.tsx
@@ -13,6 +13,11 @@ import {
 } from '@/app/components/ui/card';
 import { Input } from '@/app/components/ui/input';
 import { Label } from '@/app/components/ui/label';
+import Link from 'next/link';
+
+const handleSubmit = () => {
+  console.log('Teste');
+};
 
 export function CardWithForm() {
   return (
@@ -40,7 +45,8 @@ export function CardWithForm() {
       <CardFooter className='flex justify-between mt-5'>
         <Button
           radius='md'
-          className='w-full bg-gradient-to-tr from-brandFirst-300 to-brandFirst-500 text-white hover:shadow-lg hover:bg-brandFirst-600 active:bg-brandFirst-700 focus:outline-none focus:ring focus:ring-brandFirst-300 uppercase'
+          className='w-full bg-gradient-to-tr from-brandFirst-300 to-brandFirst-500 text-white font-semibold text-lg hover:shadow-lg hover:bg-brandFirst-600 active:bg-brandFirst-700 focus:outline-none focus:ring focus:ring-brandFirst-300 uppercase'
+          onClick={handleSubmit}
         >
           Entrar
         </Button>
