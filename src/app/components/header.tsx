61b38b3e8551d82eab5a3632838d4f9eba9854e9
Vinicius Mendes <vinimendesdev@gmail.com>
2024-01-15 12:05:11 -0300

    style: Implemented improvements to the styling of the Header component

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,29 +1,29 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import WhiteLogo from '/public/images/logo/white-logo.png';
-import DynamicTooltip from './ui/dynamic-tooltip';
-import Link from 'next/link';
+import WhiteLogo from "/public/images/logo/white-logo.png";
+import DynamicTooltip from "./ui/dynamic-tooltip";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className='bg-brandFirst-500 w-full h-16 flex justify-center items-center'>
-      <div className='container mx-auto'>
-        <nav className='flex items-center justify-between'>
-          <Link href='/'>
+    <div className="flex h-16 w-full items-center justify-center bg-brandFirst-500">
+      <div className="container mx-auto">
+        <nav className="flex items-center justify-between">
+          <Link href="/">
             <Image
               src={WhiteLogo}
-              alt='Logotipo Castro Naves'
+              alt="Logotipo Castro Naves"
               width={0}
               height={0}
-              sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-              className='w-52 h-12'
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="h-12 w-52"
             />
           </Link>
 
-          <div className='text-3xl text-white flex flex-col justify-center items-center'>
+          <div className="flex flex-col items-center justify-center text-3xl text-white">
             <DynamicTooltip
-              titleText='Sair do sistema'
-              subTitleText='Clique aqui para sair da sua conta.'
+              titleText="Sair do sistema"
+              subTitleText="Clique aqui para sair da sua conta."
             />
           </div>
         </nav>
